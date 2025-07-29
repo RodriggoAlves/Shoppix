5cb8072e3f935661312df3757d580ff89a8cd9ea
Rodriggo Alves <163574644+RodriggoAlves@users.noreply.github.com>
2025-07-29 15:29:00 -0300

    Feito a adição da tela de cadastro de produto nas Routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Login from "./pages/Login/Login"
 import ResetarSenha from "./pages/ResetarSenha/ResetarSenha";
 import Registrar from "./pages/Registrar/Registrar"
-
+import CadastrarProduto from "./pages/CadastrarProduto/CadastrarProduto"
 
 function App() {
   return (
@@ -16,6 +16,8 @@ function App() {
         <Route path="/resetar-senha" element={<ResetarSenha/>}/>
 
         <Route path="/cadastrar-usuario" element={<Registrar/>}/>
+
+        <Route path="/cadastrar-produto" element={<CadastrarProduto/>}/>
 
       </Routes>
     </Router>
